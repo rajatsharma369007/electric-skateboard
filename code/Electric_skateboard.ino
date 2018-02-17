525a23bce7e9ee1fd576e3abcef59dab02078810
Rajat sharma <rajatsharma369007@gmail.com>
2018-02-17 15:31:08 +0530

    Update Electric_skateboard.ino

--- code/Electric_skateboard.ino
+++ code/Electric_skateboard.ino
@@ -1,64 +1,42 @@
-// Control servo motor with phone's accelerometer using RoboRemo app
-// www.roboremo.com
-
-// Hardware setup:
-// BT module   Arduino
-// GND ------- GND
-// VCC ------- 5V
-// TX-O ------ pin0
-// RX-I ------ pin1
-
-// Servo       Arduino
-// GND ------- GND
-// VCC ------- VCC
-// signal ---- pin4
-
 #define bluetooth Serial
 #include <Servo.h>
+Servo myServo;   
 
-Servo myServo;
-
-char cmd[100];
-int cmdIndex;
-int led = 5;
+char cmd[100];    //for storing the character sent one byte via HC05
+int cmdIndex;     
+int led = 5;      //for headlamps
 
 
+// executing the speed control and led control
 void exeCmd() {
-
+  // for led control, here led0 and led 1 are the assigned id
   if(strcmp(cmd, "led 0")==0) digitalWrite(led, LOW);
   if(strcmp(cmd, "led 1")==0) digitalWrite(led, HIGH);
   
+  //for speed control, here x is id assigned in the app
   if(cmd[0]=='x' && cmd[1]==' ') {
     
     // value starts at cmd[2]
     // atof converts string to float
     
     float val = atof(cmd+2);
-    
-   
+    // val ranging from -255 to 255
     val = val*2;
     
     int servoMicroseconds = 1500+val;
     
-    myServo.writeMicroseconds(servoMicroseconds);      
+    myServo.writeMicroseconds(servoMicroseconds);   // values ranging from 1000 to 2000   
   }   
 }
 
 
 
 void setup() {
-  
-  delay(500); // wait for bluetooth module to start
-
   bluetooth.begin(9600); // Bluetooth default baud is 115200
   
-  myServo.attach(4, 1000, 2000);
-  
-  cmdIndex = 0;
-
-  delay(250);
-
-pinMode(led, OUTPUT);
+  myServo.attach(4, 1000, 2000);  // ESC input connected at pin 4
+  cmdIndex = 0;       
+  pinMode(led, OUTPUT);
   digitalWrite(led, LOW);
   
 }
@@ -78,8 +56,6 @@ void loop() {
       cmd[cmdIndex] = c;
       if(cmdIndex<99) cmdIndex++;
     }
-   
-    
   }
   
 }
